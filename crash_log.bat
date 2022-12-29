95f32b29c9b8041addffdf2268762b39a4ade011
Gitwiebel <120677788+Gitwiebel@users.noreply.github.com>
2022-12-29 22:36:41 +0100

    Update crash_log.bat

--- crash_log.bat
+++ crash_log.bat
@@ -1,2 +1,2 @@
-mshta "about:<script>alert('Your background is mine forever, I stick around :)');close()</script>"
+mshta "about:<script>alert('Your background is mine forever, I really stick around :)');close()</script>"
 exit
